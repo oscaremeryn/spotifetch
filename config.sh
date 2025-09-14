5a1ab64d7d1b486c9bae61ec3a96ba5b2f9417cd
Oscar Emeryn <oscaremeryn@duck.com>
2025-09-15 00:33:07 +0300

    Add config

--- config.sh
+++ config.sh
@@ -1,2 +1,12 @@
 
-VENV_PATH=""
+# Location of a python virtualenv with the requirements.txt installed
+export VENV_PATH=""
+
+# Directory to store spotifetch result products
+export PRODUCTS_DIR=""
+
+# see https://github.com/yt-dlp/yt-dlp/wiki/Extractors#exporting-youtube-cookies for an effective way to fetch cookies
+export COOKIES_FILE=""
+
+# Acquired from the 'access_token_cookie' cookie in Swing Music
+export SWING_ACCESS_TOKEN=""
